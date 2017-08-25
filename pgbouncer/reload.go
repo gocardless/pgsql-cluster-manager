c75c639508929187caad073e31efd434274b4fa4
Lawrence Jones <lawrjone@gmail.com>
2017-08-25 16:38:59 +0100

    pgbouncer.Reload to issue SIGHUP via Pg
    Use the PG socket connection to issue a configuration reload to the
    local PGBouncer service.

---
+++ pgbouncer/reload.go
@@ -0,0 +1,29 @@
+package pgbouncer
+
+import (
+	"time"
+
+	"github.com/gocardless/pgsql-novips/util"
+)
+
+// Reload will cause PGBouncer to reload configuration and live apply setting changes
+func Reload(b PGBouncer, timeout time.Duration) error {
+	psql, err := b.Psql(timeout)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = psql.Exec(`RELOAD;`)
+
+	if err == nil {
+		return nil
+	}
+
+	return util.NewErrorWithFields(
+		"failed to reload PGBouncer",
+		map[string]interface{}{
+			"error": err.Error(),
+		},
+	)
+}
