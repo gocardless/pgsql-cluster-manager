0ad4af59045634b4827908163376e1f1e720b811
Chris Sinjakli <chris@gocardless.com>
2017-10-26 15:43:02 +0100

    Add VIPs

--- docker/postgres-member/start-cluster.bash
+++ docker/postgres-member/start-cluster.bash
@@ -107,8 +107,10 @@ function configure_corosync() {
   cat <<EOF | crm configure
 property stonith-enabled=false
 property default-resource-stickiness=100
+primitive PostgresqlVIP ocf:heartbeat:IPaddr2 params ip=172.17.0.99 cidr_netmask=32 op monitor interval=5s
 primitive Postgresql ocf:heartbeat:pgsql \
     params pgctl="/usr/lib/postgresql/9.4/bin/pg_ctl" psql="/usr/bin/psql" \
+    master_ip="172.17.0.99" \
     pgdata="/var/lib/postgresql/9.4/main/" start_opt="-p 5432" rep_mode="sync" \
     node_list="pg01 pg02 pg03" primary_conninfo_opt="keepalives_idle=60 keepalives_interval=5 \
     keepalives_count=5" repuser="postgres" tmpdir="/var/lib/postgresql/9.4/tmp" \
@@ -122,6 +124,9 @@ primitive Postgresql ocf:heartbeat:pgsql \
     op stop timeout="60s" interval="0s" on-fail="block" \
     op notify timeout="60s" interval="0s"
 ms msPostgresql Postgresql params master-max=1 master-node-max=1 clone-max=3 clone-node-max=1 notify=true
+colocation vip-with-master inf: PostgresqlVIP msPostgresql:Master
+order start-vip-after-postgres inf: msPostgresql:promote PostgresqlVIP:start symmetrical=false
+order stop-vip-after-postgres 0: msPostgresql:demote PostgresqlVIP:stop symmetrical=false
 commit
 end
 EOF
